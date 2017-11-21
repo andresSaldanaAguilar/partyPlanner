eb7f3207a384aae126cf2ed2e80a83210dea129c
Andres Saldana <pollyketpo@gmail.com>
2017-11-21 15:43:33 -0600

    Añadiendo registro y login, trabajando en websql

--- scripts.js
+++ scripts.js
@@ -4,7 +4,6 @@ function createDB() {
 	db.transaction(function (tx) {
 	   tx.executeSql('CREATE TABLE IF NOT EXISTS USER (password, email)');
 	});
-	alert("creacion de bd exitosa");
 }
 
 function insertDB(){
@@ -17,6 +16,61 @@ function insertDB(){
 	alert("insercion exitosa");
 }
 
-function searchDB(){
-	
+function errorHandler(transaction, error)
+{
+    // error.message is a human-readable string.
+    // error.code is a numeric error code
+    alert('Oops.  Error was '+error.message+' (Code '+error.code+')');
+ 
+    // Handle errors here
+    var we_think_this_error_is_fatal = true;
+    if (we_think_this_error_is_fatal) return true;
+    return false;
 }
+ 
+function dataHandler(transaction, results)
+{
+    // Handle the results
+    if(results.rows==0){
+    	alert("No hay coincidencias")
+    }
+    else
+    for (var i=0; i<results.rows.length; i++) {
+        // Each row is a standard JavaScript array indexed by
+        // column names.
+        var row = results.rows.item(i);
+        string = string + row['email'] + " (Contraseña "+row['password']+")\n";
+    }
+    alert(string);
+}
+
+
+function searchDB(){
+	var password=document.getElementById("contrasenia").value;
+    db.transaction(
+        function (transaction) {
+            transaction.executeSql("SELECT * from user where password='"+password+"';",
+                [], // array of values for the ? placeholders
+                dataHandler, errorHandler);
+    }
+);
+    
+}
+	/*
+	db.transaction(function (tx) {
+   	tx.executeSql('SELECT * FROM USER', [], function (tx, results) {
+	      var len = results.rows.length, i;
+	      for (i = 0; i < len; i++){
+	         alert(results.rows.item(i).email);
+	      }	
+	   	}, null);
+	});
+	alert("lololo");
+	*/
+/*
+	db.transaction(function (transaction) {
+        transaction.executeSql("UPDATE people set shirt=? where name=?;",
+            [ shirt, name ]); // array of values for the ? placeholders
+    }, myTransactionErrorCallback, myTransactionSuccessCallback
+);
+*/
