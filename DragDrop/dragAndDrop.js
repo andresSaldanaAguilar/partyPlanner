fbbe3f9e213d9b8fd2bebd180bcca42bc14df8dd
Aristides Ariza <arizamtz2303@gmail.com>
2017-11-27 12:24:32 -0600

    Parte solicitada lista, restan pequeñas validaciones

--- DragDrop/dragAndDrop.js
+++ DragDrop/dragAndDrop.js
@@ -1,5 +1,6 @@
 /*ev es el objeto que desencadena el evento*/
 var contador =1;
+var PosicionesTabla = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 //Especifica que datos debemos arrastrar
 function drag(ev) {
 	//Especificamos tipo de datos a mover
@@ -15,16 +16,54 @@ function drop(ev) {
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
 }
-function clonar(ev){
+function clonar(ev,id){
 	ev.preventDefault(); //Evitamos que el navegador realice su comportamiento base
-	var elementoArrastrado = document.getElementById(ev.dataTransfer.getData("text")); // Elemento arrastrado
-	var elementoClonado = elementoArrastrado.cloneNode(true); // Se clona el elemento
-			elementoClonado.id = "ElemClonado" + contador; // Se cambia el id porque tiene que ser unico
-				contador += 1;	
+	if(RevisarTabla(id)==0){
+		var elementoArrastrado = document.getElementById(ev.dataTransfer.getData("text")); // Elemento arrastrado
+		var elementoClonado = elementoArrastrado.cloneNode(true); // Se clona el elemento
+			elementoClonado.id = elementoArrastrado.id +" "+contador; // Se cambia el id porque tiene que ser unico
+				contador += 1;
+				alert(elementoClonado.id);
 		ev.target.appendChild(elementoClonado); // Se añade el elemento clonado
+		MarcarTabla(id,elementoArrastrado.id);	
+	}
 }
 function eliminar(ev){
-			var elementoArrastrado = document.getElementById(ev.dataTransfer.getData("text")); // Elemento arrastrado
-			elementoArrastrado.parentNode.removeChild(elementoArrastrado); // Elimina el elemento
-			ev.target.style.border = '';   // Quita el borde
+		var elementoArrastrado = document.getElementById(ev.dataTransfer.getData("text")); // Elemento arrastrado
+		elementoArrastrado.parentNode.removeChild(elementoArrastrado); // Elimina el elemento
+			alert(elementoArrastrado.parentNode.id);
+}
+
+function MarcarTabla(idT,idA){
+	if(idA=="drag1"){
+		PosicionesTabla[idT]=1;
+	}
+	else if(idA=="drag2"){
+		PosicionesTabla[idT]=2;
+	}
+	else if(idA=="drag3"){
+		PosicionesTabla[idT]=3;
+	}
+	else if(idA=="drag5"){
+		PosicionesTabla[idT]=5;
+	}
+	else if(idA=="drag6"){
+		PosicionesTabla[idT]=6;
+	}
+	else if(idA=="drag7"){
+		PosicionesTabla[idT]=7;
+	}
+}
+function RevisarTabla(idT){
+
+		return PosicionesTabla[idT];
+}
+
+function ConfTable(){
+	alert(PosicionesTabla);
+
+}
+
+function openVentana(){
+	$(".ventana").slideDown(300);
 }
