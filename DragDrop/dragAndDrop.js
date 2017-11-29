75ebddf5d191e0d5dfb2d92db88ae9e979b62316
Andres Saldaña <andressaldana@Andress-MacBook-Pro.local>
2017-11-28 23:45:59 -0600

    lista

--- DragDrop/dragAndDrop.js
+++ DragDrop/dragAndDrop.js
@@ -1,6 +1,12 @@
 /*ev es el objeto que desencadena el evento*/
 var contador =1;
 var PosicionesTabla = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+var idObj;
+var posicion;
+var evnt;
+var pos;
+var email = sessionStorage.getItem('email');
+
 //var numCuadro;
 //Especifica que datos debemos arrastrar
 function drag(ev) {
@@ -28,20 +34,201 @@ function clonar(ev,id){
 		ev.target.appendChild(elementoClonado); // Se añade el elemento clonado
 		MarcarTabla(id,elementoArrastrado.id);
 	}
-  guardar(ev.dataTransfer.getData("text"),id);
+  idObj=ev.dataTransfer.getData("text");
+  posicion=id;
+  evnt=ev;
+  find();
+}
+//busca si existe algun objeto en la posicion
+function find(){
+	db.transaction(
+  		function (transaction) {
+					transaction.executeSql('SELECT * from object where posicion="'+posicion+'" and email="'+email+'";',
+							[], // array of values for the ? placeholders
+							saveObjHandler, errorHandler);
+	});
+}
+//busca la posicion del objeto
+function Button(posi){
+  pos = posi.parentNode.id;
+}
+//inserta y actualiza los objetos
+function insertObjData(p){
+	db.transaction(
+  		function (transaction) {
+					transaction.executeSql('SELECT * from objectdata where idPos="'+pos+'" and email="'+email+'";',
+							[], pullObjDataHandler, errorHandler);
+	});
+}
+//manejador de datos de mesas
+function pullObjDataHandler(transaction, results) {
+  var platillo=document.getElementById("event").value;
+  var mesero=document.getElementById("mesero").value;
+
+  if(results.rows.length==0){
+    db.transaction(function (tx) {
+            tx.executeSql('INSERT INTO OBJECTDATA (email,idPos,mesero,platillo) VALUES ("'+email+'","'+pos+'", "'+mesero+'", "'+platillo+'");',[],null, errorHandler);
+        });
+    //alert("insercion exitosa");
+  }
+  else{
+    db.transaction(function (tx) {
+            tx.executeSql('UPDATE OBJECTDATA SET mesero="'+mesero+'", platillo="'+platillo+'" where idPos="'+pos+'" and email="'+email+'";',[],null, errorHandler);
+    });
+    //alert("actualizacion exitosa");
+  }
+}
+//busca la info de las mesas
+function findData(p){
+	db.transaction(
+  		function (transaction) {
+					transaction.executeSql('SELECT * from objectdata where idPos="'+pos+'" and email="'+email+'";',
+							[], pushObjDataHandler, errorHandler);
+	});
+}
+//manejador de datos de mesas
+function pushObjDataHandler(transaction, results) {
+  var row = results.rows.item(0);
+  document.getElementById("event").value=row['platillo'];
+  document.getElementById("mesero").value=row['mesero'];
+}
+//busca si existe algun objeto en la posicion
+function findAll(){
+	db.transaction(
+  		function (transaction) {
+					transaction.executeSql('SELECT * from object where email="'+email+'";',
+							[], pullObjHandler, errorHandler);
+	});
+}
+//manejador de errores
+function errorHandler(transaction, error){
+    alert('Oops.  Error was '+error.message+' (Code '+error.code+')');
+}
+//guarda posicion e identificador de objeto
+function saveObjHandler(transaction, results){
+    if(results.rows.length==0){
+      db.transaction(function (tx) {
+              tx.executeSql('INSERT INTO OBJECT (email,id,posicion) VALUES ("'+email+'","'+idObj+'", "'+posicion+'");',[],null, errorHandler);
+          });
+      //alert("insercion exitosa");
+    }
+    else{
+      db.transaction(function (tx) {
+							tx.executeSql('UPDATE OBJECT SET id="'+idObj+'" where posicion="'+posicion+'" and email="'+email+'";',[],null, errorHandler);
+			});
+      //alert("actualizacion exitosa");
+    }
+
+}
+//trae los objetos
+function pullObjHandler(transaction, results) {
+  if(results.rows.length!=0){
+    var pos=[];
+    var noObj=[];
+
+    for (var i = 0; i < results.rows.length; i++) {
+      var row = results.rows.item(i);
+      pos[i]=row['posicion'];
+      noObj[i]=row['id'];
+    }
+    for (var i = 0; i < pos.length; i++) {
+      createObj(noObj[i],pos[i]);
+    }
+  }
+}
+//clona los objetos
+function createObj(noObj,pos){
+  var itm = document.getElementById(noObj);
+  var cln = itm.cloneNode(true);
+  document.getElementById(pos).appendChild(cln);
 }
 
-function guardar(id,posicion){
+//insertar invitaos
+function insertGuest(){
+  var nombre=document.getElementById("nombre").value;
   db.transaction(function (tx) {
-          tx.executeSql('INSERT INTO OBJECT (id,posicion) VALUES ("'+id+'", "'+posicion+'")',[],null, errorHandler);
+          tx.executeSql('INSERT INTO GUEST (email,idPos,nombre) VALUES ("'+email+'","'+pos+'", "'+nombre+'");',[],null, errorHandler);
       });
-  alert("insercion exitosa");
+  //alert("insercion exitosa");
+  findGuest();
+}
+
+function findGuest(){
+	db.transaction(
+  		function (transaction) {
+					transaction.executeSql('SELECT * from guest where idPos="'+pos+'" and email="'+email+'";',
+							[], showGuest, errorHandler);
+	});
+}
+
+//trae los objetos
+function showGuest(transaction, results) {
+  var myNode = document.getElementById("carta");
+  while (myNode.firstChild) {
+      myNode.removeChild(myNode.firstChild);
+  }
+
+  if(results.rows.length!=0){
+    var nombre=[];
+    var posc=[];
+    for (var i = 0; i < results.rows.length; i++) {
+      var row = results.rows.item(i);
+      nombre[i]=row['nombre'];
+      posc[i]=row['idPos'];
+    }
+    for (var i = 0; i < posc.length; i++) {
+      createGuest(nombre[i],posc[i]);
+    }
+  }
+}
+
+function createGuest(nombre,posc){
+  var itm1 = document.createElement("BUTTON");
+  var itmtxt1 = document.createTextNode("X");
+  itm1.setAttribute("class", "btn btn-danger form-control col-2");
+  itm1.setAttribute("onclick", "deleteGuest(this)");
+  itm1.setAttribute("id", nombre);
+  itm1.appendChild(itmtxt1);
+
+  var itm2 = document.createElement("h4");
+  itm2.setAttribute("class", "col-6");
+  var itmtxt2 = document.createTextNode(nombre);
+  itm2.appendChild(itmtxt2);
+
+  var div = document.createElement("div");
+  div.setAttribute("class", "row");
+  div.setAttribute("style", "margin-top:5px;");
+  div.appendChild(itm2);
+  div.appendChild(itm1);
+
+  var carta= document.getElementById("carta");
+  carta.appendChild(div);
+}
+
+function deleteGuest(who){
+  var id=who.id;
+  db.transaction(
+      function (transaction) {
+          transaction.executeSql('DELETE from guest where nombre="'+id+'";',
+              [], showGuest, errorHandler);
+  });
+  findGuest();
+}
+
+//tira la tabla de objetos
+function dropDatabaseObj(){
+    db.transaction(
+    function (transaction) {
+            transaction.executeSql('drop table object',[],null, errorHandler);
+            transaction.executeSql('drop table objectdata',[],null, errorHandler);
+            transaction.executeSql('drop table guest',[],null, errorHandler);
+    });
+    alert("database dropped");
 }
 
 function eliminar(ev){
 		var elementoArrastrado = document.getElementById(ev.dataTransfer.getData("text")); // Elemento arrastrado
 		elementoArrastrado.parentNode.removeChild(elementoArrastrado); // Elimina el elemento
-			alert(elementoArrastrado.parentNode.id);
 }
 
 function MarcarTabla(idT,idA){
@@ -70,14 +257,14 @@ function RevisarTabla(idT){
 }
 
 function ConfTable(){
-	alert(PosicionesTabla);
+
 
 }
 
 function openVentana(){
-	$(".ventana").slideDown(300);
+	$(".ventana").fadeIn(300);
 }
 
 function closeVentana(){
-	$(".ventana").slideUp(300);
+	$(".ventana").fadeOut(300);
 }
---
+++ eventScripts.js
@@ -0,0 +1,100 @@
+var email = sessionStorage.getItem('email');
+//controlador de platillos que se muestran
+function showMenu(){
+    var myNode = document.getElementById("event");
+    while (myNode.firstChild) {
+        myNode.removeChild(myNode.firstChild);
+    }
+
+    var horario=document.getElementById("hour").value;
+
+    if(horario=="dia"){
+        var opt = document.createElement("option");
+        var textnode = document.createTextNode("Omelette a la Francesa");
+        opt.appendChild(textnode);
+        opt.setAttribute("id", "1");
+        document.getElementById("event").appendChild(opt);
+
+        var opt2 = document.createElement("option");
+        var textnode2 = document.createTextNode("Chilaquiles con milanesa");
+        opt2.appendChild(textnode2);
+        opt2.setAttribute("id", "2");
+        document.getElementById("event").appendChild(opt2);
+    }
+    else if(horario=="tarde"){
+        var opt = document.createElement("option");
+        var textnode = document.createTextNode("Milanesa a la vinagreta");
+        opt.appendChild(textnode);
+        opt.setAttribute("id", "1");
+        document.getElementById("event").appendChild(opt);
+
+        var opt2 = document.createElement("option");
+        var textnode2 = document.createTextNode("Pollo envinado");
+        opt2.appendChild(textnode2);
+        opt2.setAttribute("id", "2");
+        document.getElementById("event").appendChild(opt2);
+    }
+    else if(horario=="noche"){
+        var opt = document.createElement("option");
+        var textnode = document.createTextNode("Trufa escarchada");
+        opt.appendChild(textnode);
+        opt.setAttribute("id", "1");
+        document.getElementById("event").appendChild(opt);
+
+        var opt2 = document.createElement("option");
+        var textnode2 = document.createTextNode("Frijoles");
+        opt2.appendChild(textnode2);
+        opt2.setAttribute("id", "2");
+        document.getElementById("event").appendChild(opt2);
+    }
+}
+
+//buscador de usuarios en la bd
+function searchEvent(){
+    db.transaction(
+        function (transaction) {
+            transaction.executeSql('SELECT * from event where email="'+email+'";',
+                [], // array of values for the ? placeholders
+                eventHandler, errorHandler);
+    });
+}
+
+function eventHandler(transaction, results){
+    //mostramos el boton de crear evento si no existe
+		var evento=document.getElementById("typeEvent").value;
+		var fecha=document.getElementById("date").value;
+		var horario= document.getElementById("hour").value;
+		var email = sessionStorage.getItem('email');
+
+		//si no hay registo, guardamos, sino, solo  actualizamos
+    if(results.rows.length==0){
+			db.transaction(function (tx) {
+							tx.executeSql('INSERT INTO EVENT (email,fecha,hora,evento) VALUES ("'+email+'", "'+fecha+'", "'+horario+'", "'+evento+'")',[],null, errorHandler);
+					});
+			alert("insercion exitosa: "+evento+" "+fecha+" "+horario);
+    }
+		else{
+
+			db.transaction(function (tx) {
+							tx.executeSql('UPDATE EVENT SET fecha="'+fecha+'",hora="'+horario+'",evento="'+evento+'" where email="'+email+'";',[],null, errorHandler);
+			});
+			alert("actualizacion exitosa: "+evento+" "+fecha+" "+horario);
+		}
+}
+
+function showEventHandler(transaction, results){
+    //mostramos el boton de crear evento si no existe
+		var row = results.rows.item(0);
+		document.getElementById("typeEvent").value=row['evento'];
+		document.getElementById("date").value=row['fecha'];
+		document.getElementById("hour").value=row['hora'];
+}
+
+function showEvent(){
+	db.transaction(
+			function (transaction) {
+					transaction.executeSql('SELECT * from event where email="'+email+'";',
+							[], // array of values for the ? placeholders
+							showEventHandler, null);
+	});
+}
